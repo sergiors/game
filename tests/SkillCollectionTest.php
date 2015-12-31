d565ad5279de647c493f266733dd06e1c10b144d
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2015-12-31 11:51:03 -0200

    Test to collection

--- tests/SkillCollectionTest.php
+++ tests/SkillCollectionTest.php
@@ -8,12 +8,27 @@ class SkillCollectionTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function shouldReturn()
+    public function shouldReturnACollection()
     {
         $collection = new SkillCollection();
         $collection[10] = 'test';
+        $collection['attack'] = true;
 
         $this->assertEquals('test', $collection[10]);
+        $this->assertCount(2, $collection);
 
+        unset($collection['attack']);
+        $this->assertCount(1, $collection);
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function shouldReturnInvalidArgumentException()
+    {
+        $collection = new SkillCollection();
+        $collection['test'];
     }
 }
